2eef0be23d82b60778d5d69ac8f6c4471edcd6fd
ismet tekin <ismettekin.dev@outlook.com>
2022-06-06 22:44:27 +0300

    Yeni ui elementleri eklendi

--- Assets/Scripts/Audio/ToggleAudio.cs
+++ Assets/Scripts/Audio/ToggleAudio.cs
@@ -1,14 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ToggleAudio : MonoBehaviour
 {
     [SerializeField] private bool toggleMusic, toggleEffects;
 
+
     public void Toggle()
     {
         if (toggleEffects) AudioManager.Instance.ToggleEffects();
         if (toggleMusic) AudioManager.Instance.ToggleMusic();
+        
     }
+
+   
 }
